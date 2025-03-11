9b0fa28b96c0dd240ebe75dcd47f60958f57e42d
Amanda <agsmarr@udel.edu>
2025-03-11 12:46:01 -0400

    Passes all tests

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,24 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    //WORKING
+    const [selectedAnswer, setSelected] = useState<string>(options[0]);
+    function updateSelected(event: React.ChangeEvent<HTMLSelectElement>) {
+        setSelected(event.target.value);
+    }
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group>
+                <Form.Select value={selectedAnswer} onChange={updateSelected}>
+                    {options.map((op: string) => (
+                        <option value={op}>{op}</option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {selectedAnswer == expectedAnswer ?
+                <span>✔️</span>
+            :   <span>❌</span>}
         </div>
     );
 }
